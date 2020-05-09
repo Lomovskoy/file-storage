d16f74e9e2b8b908619d4d5c40af55ae49417dd4
Lomovskoy <lomovskoy.kirill@yandex.ru>
2020-05-09 20:50:57 +0300

    #1 Сделал каркас приложения

---
+++ src/main/java/ru/animal/shelter/manager/filestorage/FileStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.animal.shelter.manager.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStorageApplication.class, args);
+    }
+
+}
